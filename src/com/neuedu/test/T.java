bf14c5e14af9276a7a55b975564ee51e50e492aa
Administrator <3091252115@qq.com>
2019-05-23 19:44:03 +0800

    test

--- src/com/neuedu/test/T.java
+++ src/com/neuedu/test/T.java
@@ -2,6 +2,12 @@ package com.neuedu.test;
 
 public class T {
     public static void main(String[] args) {
-        System.out.println(1);
+        int i = 583;
+        int m = 0;
+        while (i != 0) {
+            m =i%10;
+            System.out.print(m);
+            i = i/10;
+        }
     }
 }
